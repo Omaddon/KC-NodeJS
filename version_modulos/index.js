ddcd6ffc6c3a6762144ef67a868db2ed22cbca5b
Miguel Jardón <migueljp83@gmail.com>
2017-05-05 05:40:44 +0200

    versionModulo terminado

--- version_modulos/index.js
+++ version_modulos/index.js
@@ -1,2 +1,49 @@
-'use strict'
+'use strict';
 
+const fs = require('fs');
+const path = require('path');
+
+// en lugar de usar readFile, podemos usar directamente require: nos lee el fichero y lo parsea si
+// es un JSON... pero de forma síncrona!!
+// const packageJSON = require('./node_modules/chance/package.json');
+
+function versionModulo (nombreModulo, callback){
+
+    const fichero = path.join('./node_modules', nombreModulo, 'package.json');
+
+    // leemos contenido de un fichero package.json
+    fs.readFile(fichero, 'utf-8', (err, data) => {
+        if (err) {
+            callback(err);
+            return;
+        }
+
+        try {
+            // parseamos el contenido del fichero convirtiendolo en un objeto (síncrono!!)
+            // usamos 'var' para que haga hoisting (const y let no lo hacen) y así que sea visible
+            // fuera del try/catch
+            var packageJSON = JSON.parse(data);
+
+        } catch (err) {
+            callback(err);
+            return;
+        }
+
+        // obtenemos la version (si no existe, tomará la opción alternativa -> || '')
+        const version = packageJSON.version || '';
+
+        // retornamos la version
+        callback(null, version);
+
+    });
+}
+
+
+versionModulo ('chance', (err, data) => {
+    if (err) {
+        console.error('Hubo un error', err);
+        return;
+    }
+
+    console.log('La versión del módulo es', data);
+});
