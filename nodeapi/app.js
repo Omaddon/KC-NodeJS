4429dcc1895d176ad7d1a75c8422c4ee24aa93bf
Miguel Jardón <migueljp83@gmail.com>
2017-05-09 21:11:23 +0200

    nodeapi inicio middlewares

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -5,10 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
+
+// __dirname es una variable reservada de Express que te da la ruta del fichero actual
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,8 +21,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+// Rutas de nuestra app
+app.use('/', require('./routes/index'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- nodeapi/routes/index.js
+++ nodeapi/routes/index.js
@@ -6,8 +6,54 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/hola', (req, res, next) => {
-  res.send('Bienvenido');
+/*
+    '/param/:id?'                   => parámetro opcional
+    '/param/:id([0-9]+)'            => parámetro con regexp
+    '/param/:id'([0-9]+)/:piso(A|B) => varios params
+*/
+router.get('/paramruta/:id', (req, res, next) => {
+  console.log('req.params', req.params);
+  res.send('Ok. Parámetro recibido: ' + req.params.id);
 });
+
+// uno o más dígitos de 0-9
+// OJO! NO es case sensitive
+router.get('/param/:id([0-9]+)/piso/:piso/puerta/:puerta(A|B|C)', (req, res, next) => {
+  console.log('Varios params', req.params);
+  res.send('ok');
+});
+
+router.get('/paramopcional/:dato?', (req, res, next) => {
+  console.log('Parámetro opcional:', req.params);
+  res.send('ok');
+});
+
+// se pueden anidar filtros en la ruta (en el browser) mediante &
+router.get('/enquerystring', (req, res, next) => {
+  console.log('req.query', req.query);
+  res.send('ok');
+});
+
+// --------------------------- GET (body) ---------------------------
+router.put('/enelbody', (req, res, next) => {
+  // req.body para acceder al body de la petición
+  // req o res, .get o .set (req.get) para acceder a la cabecera de la petición
+  console.log('req.body', req.body);
+  res.send('ok en el body');
+});
+
+/*        MÉTODOS DE RESPUESTA
+
+  res.download()
+  res.end()
+  res.json()
+  res.jsonp()
+  res.redirect()
+  res.render()
+  res.send()
+  res.sendFile
+  res.sendStatus()
+*/
+
 
 module.exports = router;
